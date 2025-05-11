d295cdd2358b21dc0601fe3a676ab412266a85a3
Raveen-IT <168619870+Raveen-IT@users.noreply.github.com>
2025-05-11 10:33:12 +0530

    Update Chatbot.tsx

--- src/pages/Chatbot.tsx
+++ src/pages/Chatbot.tsx
@@ -303,7 +303,7 @@ const Chatbot = () => {
               />
               
               <div className={`${selectedCategory ? "col-span-3" : "col-span-4"} flex flex-col h-full bg-background`}>
-                <CardContent className="flex-1 p-0 overflow-auto flex flex-col">
+                <CardContent className="flex-1 overflow-auto flex flex-col p-0">
                   <MessageList
                     messages={messages}
                     isLoading={isLoading}
